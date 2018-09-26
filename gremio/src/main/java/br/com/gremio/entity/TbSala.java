b4c7d7c105f494158b4d729553f251fca5ad2bb0
re91532z <yago.garcia@la.logicalis.com>
2018-09-26 13:55:58 -0300

    Criando tabela de sala

--- gremio/src/main/java/br/com/gremio/entity/TbSala.java
+++ gremio/src/main/java/br/com/gremio/entity/TbSala.java
@@ -1,4 +1,22 @@
 package br.com.gremio.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_sala")
 public class TbSala {
+    @Id
+    @SequenceGenerator(name = "id_salaSequence", sequenceName = "id_salaSequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_salaSequence")
+    @Column(name = "id_sala", unique = true)
+    private int idSala;
+
+    @Column(name = "numero", nullable = false)
+    private int numero;
+
+    @Column(name = "capacidade", nullable = false)
+    private int capacidade;
+
+    @Column(name = "disponibilidade", nullable = false)
+    private Boolean disponibilidade;
 }
