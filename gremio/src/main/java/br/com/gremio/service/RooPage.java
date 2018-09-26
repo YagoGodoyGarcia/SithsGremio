53dc4975802e9841fb3f978e42cd86a70001e999
re91532z <yago.garcia@la.logicalis.com>
2018-09-26 13:49:37 -0300

    Bem vindo

---
+++ gremio/src/main/java/br/com/gremio/service/RooPage.java
@@ -0,0 +1,14 @@
+package br.com.gremio.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RooPage {
+
+    @RequestMapping(value="/")
+    @ResponseBody
+    public String startWebService(){
+        return "Bem-Vindo ao web service Gremio";
+    }
+}
