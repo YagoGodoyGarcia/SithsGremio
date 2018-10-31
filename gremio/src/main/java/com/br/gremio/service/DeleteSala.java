92c260a1e0f672ea976f7ada7c7d9648e60ae6ff
Fabio Augusto De Araujo Santos <fasantos2@latam.stefanini.com>
2018-10-30 21:03:13 -0300

    aa

--- gremio/src/main/java/com/br/gremio/service/DeleteSala.java
+++ gremio/src/main/java/com/br/gremio/service/DeleteSala.java
@@ -1,6 +1,8 @@
 package com.br.gremio.service;
 
 import com.br.gremio.entity.TbSala;
+
+import org.omg.PortableInterceptor.TRANSPORT_RETRY;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +17,12 @@ public class DeleteSala {
     public String deleteSala(@RequestParam Long id_sala){
         TbSala sala = salaService.getOne(id_sala);
         try {
-            salaService.delete(id_sala);
-            return "Ok";
+        	try {
+        		salaService.delete(id_sala);
+                return "Ok";
+        	}catch (Exception e) {
+        		return "Sala em um eveto";
+        	}
         }catch (Exception e){
             return "Não Encontrado!";
         }
